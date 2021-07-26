2ac58d9a5899fc27a75a81c8cf442ecbd9f326e4
Doyeop Kwon <kwon32220@gmail.com>
2021-07-26 16:02:21 +0900

    chore: miraiaDB 설정

--- backend/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-insert into user values (1,'SSAFY','홍길동','$2a$10$0sNlKs6TUMs4hTLydDpC4.LWFpzb4dY20ZYNEegPKHkeEMqvyk85S','교육생','test-1');
-insert into conference_category values(1,'업무');
-insert into conference_category values(2,'교육');
-insert into conference_category values(3,'기타');
