41b4e6885ad1ebebb1b5497d16cba1ddd7cc8c66
xoxaeux <striplehs@ajou.ac.kr>
2021-08-11 10:24:16 +0900

    _

--- backend/auth/src/main/java/com/ssafy/common/model/UserTypeEnum.java
+++
@@ -1,5 +0,0 @@
-package com.ssafy.common.model;
-
-public enum UserTypeEnum {
-	CUSTOMER, SELLER, PICKUP
-}
--- backend/auth/src/main/java/com/ssafy/security/OAuthProvider.java
+++
@@ -1,5 +0,0 @@
-package com.ssafy.security;
-
-public enum OAuthProvider {
-    google, kakao
-}
