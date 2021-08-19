9931e220e67948d4c963d982d0132a26edb530a4
cherrytomato1 <terec@naver.com>
2021-08-20 05:56:19 +0900

    feat: 주문 목록 조회 UI

--- frontend/src/components/mypage/OrderList.vue
+++ frontend/src/components/mypage/OrderList.vue
@@ -1,12 +1,8 @@
 <template>
-  <v-app
-    class="pa-2 color5"
-  >
+  <v-app class="pa-2 color5">
     <v-container>
       <v-row>
-        <v-col
-          cols="8"
-        >
+        <v-col cols="8">
           <h2>
             주문 내역
           </h2>
@@ -25,68 +21,35 @@
           />
         </v-col> -->
       </v-row>
-      <v-col
-        cols="12"
-        class="py-2"
-      >
-        <v-btn-toggle
-          v-model="text"
-          tile
-          color="deep-purple accent-3"
-          group
-        >
-          <v-btn value="left">
-            최근 6개월
-          </v-btn>
-          <v-btn value="center">
-            2021
-          </v-btn>
-          <v-btn value="right">
-            2020
-          </v-btn>
-          <v-btn value="justify">
-            2019
+      <v-col cols="12" class="py-2">
+        <v-btn-toggle v-model="tab" tile color="deep-purple accent-3" group>
+          <v-btn v-for="tabItem in tabs" :key="tabItem">
+            {{ tabItem }}
           </v-btn>
         </v-btn-toggle>
       </v-col>
     </v-container>
     <!-- outlined 적용이 잘 안되는데 다시 적용해보기 -->
-    <v-container>
+    <v-container v-for="order in $store.getters.orderList" :key="order.id">
       <v-row>
-        <v-col
-          cols="4"
-        >
+        <v-col cols="4">
           <!-- 주문 날짜 불러오기 -->
           <p>
-            <!-- {{ $store.getters.orderList.orderItemList[0].registerTime }} -->
-            <!-- {{ $store.getters.frequentStore[index].store.name }} -->
-            <!-- {{ $store.getters.orderList.orderInfo }} -->
-            <!-- {{ $store.getters.orderList }} -->
-            2021.07.23 주문
+            {{ order.registerTime.split(" ")[0] }}
           </p>
         </v-col>
-        <v-col
-          cols="2"
-        >
-          <v-row
-            row="2"
-          >
-            <p>싸피시장</p>
+        <v-col cols="2">
+          <v-row row="2">
+            <p>{{ order.market.name }}</p>
           </v-row>
         </v-col>
 
-        <v-col
-          cols="3"
-        >
-          <p>
-            106,000원
-          </p>
+        <v-col cols="3">
+          <p>{{ order.price }} 원</p>
         </v-col>
-        <v-col
-          cols="3"
-        >
+        <v-col cols="3">
           <p>
-            결제 확인
+            {{ order.orderStatus.name }}
           </p>
         </v-col>
         <!-- <v-col
@@ -99,117 +62,102 @@
           </v-btn>
         </v-col> -->
       </v-row>
-    </v-container>
-    <div
-      v-for="(fs, index) in orderList"
-      :key="index"
-    >
-      <v-form>
-        <v-container>
-          <v-row>
-            <v-col
-              cols="2"
-            >
-              <img
-                src="@/assets/fish_store.png"
-                alt="가게 사진"
-                style="width:60px"
-              >
-            </v-col>
+      <div v-for="orderItem in order.orderItemList" :key="orderItem.id">
+        <v-form>
+          <v-container>
+            <v-row>
+              <v-col cols="2">
+                <img
+                  v-bind:src="orderItem.item.image"
+                  alt="상품 사진"
+                  style="width:60px"
+                />
+              </v-col>
 
-            <v-col
-              cols="4"
-            >
-              <!-- <p>
+              <v-col cols="4">
+                <!-- <p>
                 민기네 수산<br>
                 고등어<br>
                 수량: 13 <br>
               </p> -->
-              {{ fs.item.name }}<br>
-            </v-col>
-            <v-col
-              cols="2"
-            >
-              <p>
-                2000원<br>
-                2021.07.23<br>
-              </p>
-            </v-col>
-            <v-col
-              cols="4"
-            >
-              <p>
-                요청사항 : 고등어 손질 부탁드려요!<br>
-              </p>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-form>
-    </div>
-
-    <v-row>
-      <v-col
-        cols="12"
-        offset="9"
-      >
-        <br>
-        <br>
-        <v-btn
-          color="success"
-          to="/finalorderdetail"
-        >
-          주문 상세보기
-        </v-btn>
-      </v-col>
-    </v-row>
+                <!-- {{ fs.item.name }}<br /> -->
+              </v-col>
+              <v-col cols="2">
+                <p>
+                  {{ orderItem.item.price * orderItem.amount }}<br />
+                  {{ order.registerTime.split(" ")[0] }}<br />
+                </p>
+              </v-col>
+              <v-col cols="4">
+                {{ orderItem.comment }}
+                <!-- <p>요청사항 : 고등어 손질 부탁드려요!<br /></p> -->
+              </v-col>
+            </v-row>
+          </v-container>
+        </v-form>
+      </div>
+      <v-row>
+        <v-col cols="12" offset="9">
+          <br />
+          <br />
+          <v-btn color="success" v-bind:to="orderDetailUrl + order.id">
+            주문 상세보기
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
 <script>
-import {mapGetters} from "vuex";
+import { mapGetters } from "vuex";
 
 // script vuetify에서 가져와야됨
 export default {
-  name: 'OrderList',
-    data() {
-    return{
+  name: "OrderList",
+  data() {
+    return {
       // 변경하기
       tab: null,
-    }
+      tabs: ["최근 6개월", "2021", "2020", "2019", "2018"],
+      orderDetailUrl: "/find-order-detail/"
+    };
   },
 
-  computed:{
+  computed: {
     ...mapGetters(["orderList", "getToken"])
   },
-  watch:{
-    tab: function (val){ // 선택한 탭 변경될 경우
-      this.$store.commit("setOrderList",this.$store.getters.orderList[val]);
-      console.log(this.$store.getters.OrderList);
+  watch: {
+    tab: function(val) {
+      // 선택한 탭 변경될 경우
+      console.log("tab >>>>> " + val);
+      // this.$store.commit("setOrderList", this.$store.getters.orderList[val]);
+      this.$store.dispatch("getOrderList", this.tabs[val]);
+      // console.log(this.$store.getters.OrderList);
     }
   },
   created() {
-    this.$store.dispatch("getOrderList");
+    this.$store.dispatch("getOrderList", this.tabs[0]);
   }
-
-}
+};
 </script>
 
 <style>
-.color1{
+.color1 {
   background-color: #cff0da !important;
 }
-.color2{
+.color2 {
   background-color: #88dba3 !important;
 }
-.color3{
+.color3 {
   background-color: #dadbdb !important;
 }
-.color4{
+.color4 {
   background-color: #3ac569 !important;
 }
-#app{
-  font-family: 'Stylish', sans-serif;
-  font-family: 'Jua', sans-serif;
-  font-family: 'Yeon Sung', cursive;
+#app {
+  font-family: "Stylish", sans-serif;
+  font-family: "Jua", sans-serif;
+  font-family: "Yeon Sung", cursive;
 }
 </style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,122 +1,120 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Mainpage from '@/views/Mainpage'
-import StoreDetail from '@/views/StoreDetail'
-import StoreManager from '@/views/StoreManager'
-import InitPage from '@/views/InitPage'
-import LiveManager from '@/views/LiveManager'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Mainpage from "@/views/Mainpage";
+import StoreDetail from "@/views/StoreDetail";
+import StoreManager from "@/views/StoreManager";
+import InitPage from "@/views/InitPage";
+import LiveManager from "@/views/LiveManager";
 
+import Basket from "@/views/Basket";
+import OrderCheck from "@/views/OrderCheck";
+import UserMypage from "@/views/UserMypage";
+import StoreMypage from "@/views/StoreMypage";
+import FrequentStoreManage from "@/views/FrequentStoreManage";
+import Payment from "@/views/Payment";
+import FinalOrderDetail from "@/views/FinalOrderDetail";
 
-import Basket from '@/views/Basket'
-import OrderCheck from '@/views/OrderCheck'
-import UserMypage from '@/views/UserMypage'
-import StoreMypage from '@/views/StoreMypage'
-import FrequentStoreManage from '@/views/FrequentStoreManage'
-import Payment from '@/views/Payment'
-import FinalOrderDetail from '@/views/FinalOrderDetail'
+import StoreInformationModify from "@/components/mypage/StoreInformationModify";
+import OrderPayment from "@/components/payment/OrderPayment";
+import OauthHandler from "@/components/login/OauthHandler";
+import Pickup from "@/views/Pickup";
 
-import StoreInformationModify from '@/components/mypage/StoreInformationModify'
-import OrderPayment from '@/components/payment/OrderPayment'
-import OauthHandler from '@/components/login/OauthHandler'
-import Pickup from '@/views/Pickup'
-
-
-``
-Vue.use(VueRouter)
+``;
+Vue.use(VueRouter);
 
 const routes = [
   {
     // 초기 화면으로 하기 위해서
-    path: '/',
+    path: "/",
     // path: '/Init',
-    name: 'InitPage',
+    name: "InitPage",
     component: InitPage
   },
   {
     // 아래와 같이하면 화면이 안나옴
     // path: '/',
-    path: '/mainpage',
-    name: 'Mainpage',
+    path: "/mainpage",
+    name: "Mainpage",
     component: Mainpage
   },
   {
-    path: '/storedetail',
-    name: 'StoreDetail',
+    path: "/storedetail",
+    name: "StoreDetail",
     component: StoreDetail
   },
   {
-    path: '/storemanager',
-    name: 'StoreManager',
+    path: "/storemanager",
+    name: "StoreManager",
     component: StoreManager
   },
   {
-    path: '/ordercheck',
-    name: 'OrderCheck',
+    path: "/ordercheck",
+    name: "OrderCheck",
     component: OrderCheck
   },
   {
-    path: '/basket',
-    name: 'Basket',
+    path: "/basket",
+    name: "Basket",
     component: Basket
   },
   {
-    path: '/frequentstoremanage',
-    name: 'FrequentStoreManage',
+    path: "/frequentstoremanage",
+    name: "FrequentStoreManage",
     component: FrequentStoreManage
   },
   {
-    path: '/mypage/storeinformationmodify',
-    name: 'StoreInformationModify',
+    path: "/mypage/storeinformationmodify",
+    name: "StoreInformationModify",
     component: StoreInformationModify
   },
   {
-    path: '/usermypage',
-    name: 'UserMypage',
+    path: "/usermypage",
+    name: "UserMypage",
     component: UserMypage
   },
   {
-    path: '/storemypage',
-    name: 'StoreMypage',
+    path: "/storemypage",
+    name: "StoreMypage",
     component: StoreMypage
   },
   {
-    path: '/orderpayment',
-    name: 'OrderPayment',
+    path: "/orderpayment",
+    name: "OrderPayment",
     component: OrderPayment
   },
   {
-    path: '/payment',
-    name: 'Payment',
+    path: "/payment",
+    name: "Payment",
     component: Payment
   },
   {
-    path: '/pickup',
-    name: 'Pickup',
+    path: "/pickup",
+    name: "Pickup",
     component: Pickup
   },
   {
-    path: '/finalorderdetail',
-    name: 'FinalOrderDetail',
+    path: "/finalorderdetail/:order-id",
+    name: "FinalOrderDetail",
     component: FinalOrderDetail
   },
   {
-    path: '/oauth2/redirect',
-    name: 'OauthHandler',
-    component: OauthHandler,
+    path: "/oauth2/redirect",
+    name: "OauthHandler",
+    component: OauthHandler
   },
   {
-    path: '/livemanager',
-    name: 'LiveManager',
-    component: LiveManager,
-  },
-]
+    path: "/livemanager",
+    name: "LiveManager",
+    component: LiveManager
+  }
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   routes
-})
+});
 
-export default router
+export default router;
 
 // export const router = createRouter({
 //   history: createWebHistory(),
